27540646571a643e3c0e550ee6cd1b9f73270b0f
davron1989 <63379195+davron1989@users.noreply.github.com>
2020-07-29 14:09:07 -0500

    Update Email.groovy

--- Email.groovy
+++ Email.groovy
@@ -1,1 +1,17 @@
-
+node {
+    stage("Stage1") {
+        echo "Hello"
+    }
+     stage("Stage2") {
+        echo "Hello"
+    }
+     stage("Stage3") {
+        echo "Hello"
+    }
+     stage("Stage4") {
+        echo "Hello"
+    }
+     stage("Send Email to Support") {
+        mail bcc" '', body: 'Running', cc: 'example@company.com', from: '', replyTo: '', subject: 'test', to: 'davron.n@yahoo.com'
+    }
+}
